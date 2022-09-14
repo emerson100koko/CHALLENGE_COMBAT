70db3c2e837d9c14cf671e825edb618fee8d5201
emerson <emerson100loko@hotmail.com>
2022-09-14 14:05:45 -0300

    chalenge combat

---
+++ src/aplication/Program.java
@@ -0,0 +1,68 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.champion;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the data of the first champion ");
+		System.out.print("Name: ");
+		String name = sc.next();
+
+		System.out.print("Initial life: ");
+		int life = sc.nextInt();
+
+		System.out.print("Attack: ");
+		int attack = sc.nextInt();
+
+		System.out.print("Armor: ");
+		int armor = sc.nextInt();
+
+		champion c1 = new champion(name, armor, attack, life);
+		
+		System.out.println();
+		System.out.println("Enter the data of the secund champion ");
+		System.out.print("Name: ");
+		name = sc.next();
+
+		System.out.print("Initial life: ");
+		life = sc.nextInt();
+
+		System.out.print("Attack: ");
+		attack = sc.nextInt();
+
+		System.out.print("Armor: ");
+		armor = sc.nextInt();
+
+		champion c2 = new champion(name, armor, attack, life);
+
+		System.out.println();
+		System.out.printf("How many turns do you want to run? ");
+		System.out.println();
+		
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			c1.takeDamage(c2);
+			c2.takeDamage(c1);
+
+			System.out.printf("Result of the recond turn: ", i +1);
+			System.out.println(c1.status());
+			System.out.println(c2.status());
+
+			if (c1.getLife() <= 0 || c2.getLife() <= 0) {
+
+				break;
+			}
+			System.out.println("COMBAT END");
+		}
+		sc.close();
+	}
+
+}
---
+++ src/entities/champion.java
@@ -0,0 +1,87 @@
+package entities;
+
+public class champion {
+	
+	private String name;
+	private int life;
+	private int attack;
+	private int armor;
+	
+	public champion(String name, int life, int attack, int armor) {
+		this.name = name;
+		this.life = life;
+		this.attack = attack;
+		this.armor = armor;
+		
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getLife() {
+		return life;
+	}
+
+	public void setLife(int life) {
+		this.life = life;
+	}
+
+	public int getAttack() {
+		return attack;
+	}
+
+	public void setAttack(int attack) {
+		this.attack = attack;
+	}
+
+	public int getArmor() {
+		return armor;
+	}
+
+	public void setArmor(int armor) {
+		this.armor = armor;
+	}
+	
+	public void champion(String name, int damage, int armor) {
+		this.armor = armor;
+		this.attack = attack;
+		this.life = life;
+		this.name = name;
+		
+	}
+	
+	public void takeDamage(champion other) {
+		if (other.getArmor() >= this.attack) {
+			other.setLife(other.getLife() - 1);
+		}
+		else {
+			other.setLife(other.getLife() + other.getArmor() - this.attack);
+		}
+		
+	}
+	
+	public String status() {
+		if (this.life <= 0) {
+			return this.name
+			   + ": ")
+			   + "0 of life (dead)";
+		}else {
+		return this.name
+			+ ": "
+			+ String.format("%d", this.life)
+			+ "of life";
+		}
+		
+	}
+	
+
+}
+	
+
+	
